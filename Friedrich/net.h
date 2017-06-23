3d0f70919894db42b7eea76c064aface60ca4117
huapuer <ucman@126.com>
2017-06-23 23:20:11 +0800

    modify net test case

--- Friedrich/net.h
+++ Friedrich/net.h
@@ -3,6 +3,9 @@
 #include "../../Ludwig/Ludwig/ludwig_net.h"
 
 void acts_test(char* c, int size) {
-	printf("From Alan£º%s \r\n", c);
-	friedrich_says(net_events::EVENT_TEST, "How are you Alan.", 17);
+	for (int i = 0; i < size; i++) {
+		printf("%c", c[i]);
+	}
+	printf("\n");
+	//friedrich_says(net_events::EVENT_TEST, "How are you Alan.", 17);
 }
